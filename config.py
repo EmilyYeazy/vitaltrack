eb42b6c7fb3cd200bd92636c9d23d33c21395c4c
EmilyYeazy <emilyyan200788@gmail.com>
2025-01-13 09:07:41 -0500

    Configuration Update for Project

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv('password.env')
 
-class Config:
+class config:
     NUTRITIONIX_APP_ID = os.getenv('NUTRITIONIX_APP_ID')
     NUTRITIONIX_API_KEY = os.getenv('NUTRITIONIX_API_KEY')
 
@@ -12,17 +12,3 @@ class Config:
 
     NUTRITION_END_POINT = "https://trackapi.nutritionix.com/v2/natural/nutrients"
     NUTRITION_DB_FILE = "data/nutrition_data.csv"
-
-    SENDER_EMAIL = os.getenv('SENDER_EMAIL')
-    SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv('password.env')
-
-# Debugging to check if the environment variables are loaded
-print("NUTRITIONIX_APP_ID:", os.getenv('NUTRITIONIX_APP_ID'))
-print("NUTRITIONIX_API_KEY:", os.getenv('NUTRITIONIX_API_KEY'))
-print("SENDER_EMAIL:", os.getenv('SENDER_EMAIL'))
-print("SENDER_PASSWORD:", os.getenv('SENDER_PASSWORD'))
